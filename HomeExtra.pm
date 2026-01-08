5b289b54cde4bc2c3d6a924838f5cc39a21ddbf9
Michael Herger <michael@herger.net>
2026-01-08 22:51:02 +0100

    Tell MaterialSkin to update the home menu item every now and then

--- HomeExtra.pm
+++ HomeExtra.pm
@@ -4,9 +4,13 @@ use strict;
 
 use base qw(Plugins::MaterialSkin::HomeExtraBase);
 
+use constant UPDATE_INTERVAL => 7200;
+
+use Slim::Utils::Log;
 use Slim::Utils::Prefs;
 
 my $prefs = preferences('plugin.1001albums');
+my $log = logger('plugin.1001albums');
 
 __PACKAGE__->initPlugin();
 
@@ -22,6 +26,17 @@ sub initPlugin {
 			needsPlayer => 1,
 		}
 	);
+
+	if (Plugins::MaterialSkin::Plugin->can('signalHomeExtraUpdate')) {
+		Slim::Utils::Timers::setTimer(undef, time() + UPDATE_INTERVAL, \&requestHomeExtrasUpdate);
+	}
+}
+
+sub requestHomeExtrasUpdate{
+	main::INFOLOG && $log->is_info && $log->info("Tell MaterialSkin to update the Home Extras");
+	Slim::Utils::Timers::killTimers(undef, \&requestHomeExtrasUpdate);
+	Plugins::MaterialSkin::Plugin::signalHomeExtraUpdate();
+	Slim::Utils::Timers::setTimer(undef, time() + UPDATE_INTERVAL, \&requestHomeExtrasUpdate);
 }
 
 1;
